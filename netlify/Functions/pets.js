3471f0fa1e99c5d971c54340e1333f120f5f7a12
Hector Percival <hp@Hectors-iMac.local>
2025-09-29 17:18:23 -0400

    first commit for using

---
+++ netlify/Functions/pets.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require("mongodb")
+
+
+const handler = async () => {
+const client = new MongoClient(process.env.CONNECTIONSTRING)
+await client.connect()
+
+const pets = await client.db().collection("pets").find().toArray()
+client.close()
+
+    return {
+statusCode: 200,
+Headers: {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+body: JSON.stringify(pets)
+}
+}
+module.exports = { handler }
+
